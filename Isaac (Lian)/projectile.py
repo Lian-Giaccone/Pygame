a503ee9bb9d3f036f84d0cacc13fb7e8222b8b7c
Lian-Giaccone <131891074+Lian-Giaccone@users.noreply.github.com>
2023-11-01 12:19:50 -0300

    Add files via upload

--- Isaac (Lian)/projectile.py
+++ Isaac (Lian)/projectile.py
@@ -1,5 +1,24 @@
 import pygame as pg
+from settings import *
 
 class Projectile:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, x, y, direction) -> None:
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.speed = 10
+        self.radio = 10
+        self.color = CELESTE
+
+    def update(self):
+        if self.direction == 'up':
+            self.y -= self.speed
+        elif self.direction == 'down':
+            self.y += self.speed
+        elif self.direction == 'left':
+            self.x -= self.speed
+        elif self.direction == 'right':
+            self.x += self.speed
+
+    def render(self, screen):
+        pg.draw.circle(screen, self.color, (self.x, self.y), self.radio)
